1360863e2aebe5628b194580da73bae0d0db70b2
Caio Henrique <caio@ganex.com.br>
2019-06-13 17:33:27 -0300

    Add CloudWatch log-group name output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "cloudtrail_arn" {
   value       = "${aws_cloudtrail.default.arn}"
   description = "The Amazon Resource Name of the trail"
 }
+
+output "cloudwatch_log_group_name" {
+  value       = "${ join("", aws_cloudwatch_log_group.cloudtrail.*.name) }"
+  description = "The CloudWatch log group name"
+}
