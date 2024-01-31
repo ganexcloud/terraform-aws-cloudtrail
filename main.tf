78107d89c04e527198d83c2e33576c6b52201ad0
caiohasouza <caio@ganex.com.br>
2024-01-31 13:02:44 -0300

    fix: test

--- main.tf
+++ main.tf
@@ -124,11 +124,18 @@ data "aws_iam_policy_document" "cloudwatch_assume_role" {
       type = "Service"
     }
 
-    condition {
-      test     = "ArnLike"
-      variable = "aws:SourceArn"
-      values   = ["arn:aws:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/${var.name}"]
-    }
+    #condition {
+    #  test     = "ArnLike"
+    #  variable = "aws:SourceArn"
+    #  values   = ["arn:aws:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/${var.name}"]
+    #}
+    #
+    #condition {
+    #  test     = "StringEquals"
+    #  variable = "aws:SourceAccount"
+    #  values   = [data.aws_caller_identity.current.account_id]
+    #}
+
   }
 }
 
